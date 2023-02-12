21b2f5a3bac32b5aad062ebc1abe6d6757cda602
Vijay <vijayganganapalli@gmail.com>
2023-02-12 21:05:44 +0530

    Updated members list tile

--- lib/screens/members_screen/members_screen.dart
+++ lib/screens/members_screen/members_screen.dart
@@ -18,7 +18,7 @@ class _MembersScreenState extends State<MembersScreen> {
     return Scaffold(
       backgroundColor: greyColor.withOpacity(0.08),
       body: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(6),
         child: StreamBuilder(
           stream: FirebaseFirestore.instance
               .collection('fpcs')
@@ -52,66 +52,72 @@ class _MembersScreenState extends State<MembersScreen> {
                 children: snapshot.data!.docs.map((document) {
                   final date = DateTime.parse(
                       document['joiningDate'].toDate().toString());
-                  return Stack(
-                    alignment: Alignment.topRight,
-                    children: [
-                      ListTile(
-                        visualDensity: VisualDensity.adaptivePlatformDensity,
-                        isThreeLine: true,
-                        tileColor: backgroundColor,
-                        contentPadding: const EdgeInsets.all(6),
-                        leading: CircleAvatar(
-                          backgroundColor: greyColor.withOpacity(0.2),
-                          radius: 30.0,
-                          child: document['memberImgUrl'] != null
-                              ? ClipRRect(
-                                  borderRadius: BorderRadius.circular(100),
-                                  child: Image.network(document['memberImgUrl'],
-                                      width: 60, height: 60, fit: BoxFit.fill),
-                                )
-                              : const Icon(Icons.person,
-                                  size: 45, color: greyColor),
-                        ),
-                        title: Text(document['fullName']),
-                        subtitle: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                "${document['maritalTitle']} ${document['fatherOrHusbandName']}"),
-                            const SizedBox(height: 2),
-                            Text(
-                                "${document['habitation']}, ${document['revenueVillage']}"),
-                            const SizedBox(height: 2),
-                            Text(document['mandal']),
-                          ],
-                        ),
-                        trailing: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              "ID: ${document['memberId'].toString()}",
-                              softWrap: true,
-                              style: TextStyle(
-                                color: defaultColor.withOpacity(0.4),
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Stack(
+                      alignment: Alignment.topRight,
+                      children: [
+                        ListTile(
+                          visualDensity: VisualDensity.adaptivePlatformDensity,
+                          isThreeLine: true,
+                          tileColor: backgroundColor,
+                          contentPadding: const EdgeInsets.all(6),
+                          horizontalTitleGap: 8,
+                          leading: CircleAvatar(
+                            backgroundColor: greyColor.withOpacity(0.2),
+                            radius: 30.0,
+                            child: document['memberImgUrl'] != null
+                                ? ClipRRect(
+                                    borderRadius: BorderRadius.circular(100),
+                                    child: Image.network(
+                                      document['memberImgUrl'],
+                                      fit: BoxFit.fill,
+                                    ),
+                                  )
+                                : const Icon(Icons.person,
+                                    size: 45, color: greyColor),
+                          ),
+                          title: Text(document['fullName']),
+                          subtitle: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  "${document['maritalTitle']} ${document['fatherOrHusbandName']}"),
+                              const SizedBox(height: 2),
+                              Text(
+                                  "${document['habitation']}, ${document['revenueVillage']}"),
+                              const SizedBox(height: 2),
+                              Text(document['mandal']),
+                            ],
+                          ),
+                          trailing: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                "ID: ${document['memberId'].toString()}",
+                                softWrap: true,
+                                style: TextStyle(
+                                  color: defaultColor.withOpacity(0.4),
+                                ),
                               ),
-                            ),
-                            Text(
-                              "SH: ${document['shareHolding']}",
-                              softWrap: true,
-                              style: TextStyle(
-                                color: defaultColor.withOpacity(0.4),
-                              ),
-                            )
-                          ],
+                              Text(
+                                "SH: ${document['shareHolding']}",
+                                softWrap: true,
+                                style: TextStyle(
+                                  color: defaultColor.withOpacity(0.4),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 12, right: 8),
-                        child: Text("${date.day}-${date.month}-${date.year}"),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12, right: 8),
+                          child: Text("${date.day}-${date.month}-${date.year}"),
+                        ),
+                      ],
+                    ),
                   );
                 }).toList(),
               );
